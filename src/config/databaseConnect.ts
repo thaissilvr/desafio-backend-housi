93aaf9e9aa38b17f695be2e0013cb5e84ca856b3
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-05-13 09:14:05 -0300

    Removendo comentários.

--- src/config/databaseConnect.ts
+++ src/config/databaseConnect.ts
@@ -1,7 +1,5 @@
 require("dotenv").config( {path: "././.env" } )
 import mongoose from "mongoose";
-// const username = 
-// const password = 
 const DB_uri: string =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@reservasbd.bxry7.mongodb.net/reservasbd`;
 
 
